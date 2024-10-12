78259eca6ca86dcacedd4935a953e21c15c63d4c
johnalysonn <johnalyson6633@gmail.com>
2024-10-12 13:09:26 -0300

    :sparkles: add validação de horas e email e cadastra proposta

---
+++ app/Livewire/Proposals/Create.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\Proposals;
+
+use App\Models\Project;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class Create extends Component
+{
+    public Project $project;
+
+    public bool $modal = false;
+
+    #[Validate(['required', 'email'])]
+    public string $email = '';
+
+    #[Validate(['required', 'numeric', 'gt:0'])]
+    public int $hours = 0;
+
+
+    public function save() {
+        $this->validate();
+
+        $this->project->proposals()
+            ->updateOrCreate(
+                ['email' => $this->email],
+                ['hours' => $this->hours]
+            );
+    }
+
+    public function render()
+    {
+        return view('livewire.proposals.create');
+    }
+
+}
--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,3 +1,3 @@
-<div>
+<div class="col-span-2">
     <x-project-card :$project />
 </div>
